308d0d679a9ad0f5cbbecca718f9441373118b0b
Chandan Soni <90852214+Chandansoni2021@users.noreply.github.com>
2022-02-10 11:42:30 +0530

    first commit

---
+++ learn_python.py
@@ -0,0 +1,184 @@
+# // # # //               # code_with_chandan
+# // # # //                                                          # code_with_chandan
+# // # # //                            # code_with_chandan ... # code_with_chandan
+
+# // # first of all we learn about function
+# // # def multiplication():
+# // #     add = 13*18
+# // #     print(add)
+# // # multiplication()
+# // # def my_function(a,b):
+# // #     add = a+b
+# // #     print(add)
+# // # my_function(4,2)
+# // # def even_odd():
+# // # #     n = int(input("Enter the value of n :-\n"))
+# // # #     if n%2 == 0:
+# // # #         print("this is even number")
+# // # #     else:
+# // # #         print("This is odd number")
+# // # # even_odd()
+# // # def persentages():
+# // #     hindi = int(input("Enter the marks of hindi subject"))
+# // #     math = int(input("Enter the marks of math  subject"))
+# // #     english = int(input("Enter the marks of  english subject"))
+# // #     physics = int(input("Enter the marks of physics subject"))
+# // #     chemistry = int(input("Enter the marks of chemistry subject"))
+# // #     per = (hindi+math+english+physics+chemistry)/500*100
+# // #     print(per)
+# // #     if per>70:
+# // #         print("First Divison")
+# // #     else:0-
+# // #         print("second diison")
+# // # def my_func():
+# // #  pass
+# // # list = [4,5,6,7,8,9,0]
+# // # list.append(67)
+# // # print(list)
+# // # arr = [4,7,6,8,9,3]
+# // # arr.pop()
+# // # print(arr)
+
+
+# // # STRING FUNCTIONS:
+
+# // # str =  "hay how are YOu"
+# // # print(str.capitalize())
+
+# // # chandan = "hw are You ChaNdan ,Whats your name "
+# // # print(chandan.upper())
+# // # print(chandan.lower())
+# // # # print(len(chandan))
+# // # def substraction(a,d):
+# // #     sub = a-d
+# // #     return sub
+
+# // # int a = 56
+# // # int d = 45
+# // # substraction(int a,int d)
+
+# // # # name = john
+# // # age = 17
+# // # Start of by creating variables
+# // # num1 = 112
+# // # num2 = 20.45
+# // # num3 = 7+3j
+# // # sum = num1+num2+num3
+# // # print(sum)
+
+# // # 1...# If we make a function program in which some numbers iseven or not 
+# // # SO LET'S START
+
+# // # def function(a,b):
+# // #   sum = a+b
+# // #   print(sum)
+# // # function(4,8)
+# // # This iss rivisin  = = == 
+
+
+# // # def even_odd(v):
+# // #     if v%2 == 0:
+# // #         print("this number is even number")
+# // #     else:
+# // #         print("This number is odd number")
+# // # even_odd(78)            
+
+# // # Second Method  == In which usser takes a number by the program :::::::::::+===
+
+# // # def function():
+# // #     num = int(input("Enter a number for checking even or od: "))
+# // #     if num%2==0:
+# // #         print("this is even number")
+# // #     else:
+# // #         print("This is odd number....") 
+# // # function()           
+    
+# // # 2.....# WE MADE A FUNCTION PROGRAM IN WHICH WE FIND THE AREA OF A CIRCLR BY AREA  = PI*R**2::
+
+
+# // # def area():
+# // #     radius = int(input("Enter the value of a radius: "))
+# // #     area  = 3.14*radius**2
+# // #     print(area)
+# // # area()
+
+
+# // # Define and Calling a function
+
+# // # def my_function():
+# // #     print("hellow world!")
+# // # my_function()
+
+
+# // #  SQUARE AND CUBE A NUMBER :::====
+# // # def square(s):
+# // #     squre = s*s
+# // #     print("the squre of this number is",squre)
+# // # square(2)    
+
+# // # PALINDROM NUMBR USING PYTHON
+
+# // # n = int(input("Enter number: "))
+# // # reverse = 0
+# // # temp = n
+# // # while(n>0):
+# // #     digit = n%10
+# // #     reverse = reverse*10 + digit
+# // #     n = n//10
+# // # if(temp == reverse):
+# // #     print("The number is a palindrom!")
+# // # else:
+# // #     print("The number is not a palindrom!")    
+ 
+# // #  In this CODE we learn about print *
+# // #                                    * *
+# // #                                    * * *
+# // #                                    * * * *
+# // # so let's start brothers""""""""""""""""|||||||||||||
+# // #IN C LANGUAGE 
+
+# // #include<stdio.h>
+# // int main()
+# // {
+# // char marks[1]=5657;
+# // char marks[0]=87;
+# // printf(marks[0]);
+# // }
+
+# // THIS IS WRONG COde
+
+# // #include<stdio.h>
+# // int main()
+
+# // {
+# //     int i=0;
+# //     int marks[5];
+# //     marks[1]=69;
+# //     marks[1]=69877;
+# //     marks[2]=66482054;
+# //     marks[3]=5830;
+# //     marks[4]=84485038;
+# //     for(i=0;i<5;i++)
+# //     {
+# //         printf("%d\n",marks[i]);
+# //     }
+# //     return 0;
+# // }
+
+# // from speedtest import speedtest
+# // st = speedtest()
+# // print("your connection's download Speed is: ", st.download())
+# // print("your conection's upload speed is: ", st.upload())
+
+# PATTERN PROGRAMM
+
+# for x in range(1,16):
+#     for j in range(1,x+1):
+#         print("*",end="")
+#     print()    
+
+
+# decimal to binary conversion
+num = (int(input("enter a number: "))
+
+print(bin(15))
