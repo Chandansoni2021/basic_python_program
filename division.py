51099c5c041566fe03142382c6737e9155a02b0b
Chandan Soni <90852214+Chandansoni2021@users.noreply.github.com>
2022-08-01 22:09:27 +0530

    division without division ooperator

---
+++ division.py
@@ -0,0 +1,16 @@
+a=int(input("enter the number"))
+b=int(input("enter the divider"))
+sign=0
+if(a<0 or b<0):
+    sign=-1
+else:
+    1
+a=abs(a)
+b=abs(b)
+quotient = 0
+while (a >= b):
+        a-= b
+        quotient += 1
+if sign == -1:
+    quotient = -quotient
+print(quotient)
